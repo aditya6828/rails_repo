f4b42153ed57a5cb8ce71e231a3214b1a8482f4a
aditya6828 <adityaanand6828@gmail.com>
2023-11-01 18:36:02 +0530

    changed the authorization isuue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def create
         user = User.find_by(username: params[:username])
 
-        if user && user.authenticate(params[:password])
+        password = params[:password]
+
+        if user && user.authenticate(password)
 
             session[:user_id] = user.id
             render json: {user_id: user.id}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,18 +14,18 @@ class UsersController < ApplicationController
   
       encrypted_password = BCrypt::Password.create(user_params[:password])
   
-      user = User.new(user_params)
-  
+      user = User.new(user_params) 
+        puts "this is user #{user.password}"
       if user.save
         render json: { message: 'User registered successfully' }
       else
-        render json: { error: 'Failed to register user' }, status: 400
+        render json: user.errors.full_messages, status: 400
       end
     end
 
     private 
     def user_params
-    params.require(:user).permit(:username, :password_digest, :email, :id, :first_name, :last_name)
+    params.permit(:username, :password, :password_confirmation, :email, :id, :first_name, :last_name)
     end 
 
 
